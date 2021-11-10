490a94e9e5eb003513affceca141ba92a9e0f8d3
Kashapov <rusl.kashapov@gmail.com>
2021-11-10 14:47:22 +0400

    nothing

--- src/main/java/ru/ssau/tk/way/practice2/arrays/Array.java
+++ src/main/java/ru/ssau/tk/way/practice2/arrays/Array.java
@@ -5,115 +5,114 @@ import java.util.ArrayList;
 public class Array {
     int[] arr;
     int i;
-    public int[] arr1 (int a){
-       return new int[a];
+
+    public int[] arr1(int a) {
+        return new int[a];
     }
 
-    public int[] arr3 (int a){
+    public int[] arr3(int a) {
         arr = new int[a];
-        for(i=0; i<a; i++){
-            arr[i]=2*i+1;
+        for (i = 0; i < a; i++) {
+            arr[i] = 2 * i + 1;
         }
         return arr;
     }
 
-    public int[] arr2 (int a){
+    public int[] arr2(int a) {
         arr = new int[a];
-        arr[0]=arr[a-1]=2;
-        for (i=1; i<a-1; i++){
-            arr[i]=1;
+        arr[0] = arr[a - 1] = 2;
+        for (i = 1; i < a - 1; i++) {
+            arr[i] = 1;
         }
         return arr;
     }
 
-    public int[] arr4(int a){
+    public int[] arr4(int a) {
         arr = new int[a];
-        for (i=0;i<a;i++){
-            arr[i]=(a-i)*2;
+        for (i = 0; i < a; i++) {
+            arr[i] = (a - i) * 2;
         }
         return arr;
     }
 
-    public int[] arr5(int a){
-        arr= new int[a];
-        arr[0]=0;
-        arr[1]=1;
-        for(i=2; i<a; i++){
-            arr[i] = arr[i-1]+arr[i-2];
+    public int[] arr5(int a) {
+        arr = new int[a];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (i = 2; i < a; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
         }
         return arr;
     }
 
     public int[] arr6(int a) {
         arr = new int[a];
-        for(i=0; i<a; i++){
-            arr[i]=(i+1)*(i+1);
+        for (i = 0; i < a; i++) {
+            arr[i] = (i + 1) * (i + 1);
         }
         return arr;
     }
 
-    public double[] arr7(double a, double b, double c){
+    public double[] arr7(double a, double b, double c) {
 
-        if (a==0){
+        var v = b * b - 4 * a * c;
+        if (a == 0) {
             double[] arr = new double[1];
-            arr[0] = (-c/b);
+            arr[0] = (-c / b);
             return arr;
-        }
-        else if(b*b-4*a*c<0){
-            double[] arr = new double[]{};
-            return arr;
-        }
-        else if(b*b-4*a*c==0){
+        } else if (v < 0) {
+            return new double[]{};
+        } else if (v == 0) {
             double[] arr = new double[1];
-            arr[0] = (-b)/(2*a);
+            arr[0] = (-b) / (2 * a);
             return arr;
         }
         double[] arr = new double[2];
-        arr[0] = (-b+Math.sqrt(b*b-4*a*c))/(2*a);
-        arr[1] = (-b-Math.sqrt(b*b-4*a*c))/(2*a);
+        arr[0] = (-b + Math.sqrt(v)) / (2 * a);
+        arr[1] = (-b - Math.sqrt(v)) / (2 * a);
         return arr;
     }
 
-    public int[] arr8(int a){
-        int k=1;
+    public int[] arr8(int a) {
+        int k = 1;
         arr = new int[a];
-        for (i=0;i<a; i++){
-            if ((i+k)%3==0){
-                k = k+1;
+        for (i = 0; i < a; i++) {
+            if ((i + k) % 3 == 0) {
+                k = k + 1;
             }
-            arr[i]=i+k;
+            arr[i] = i + k;
         }
         return arr;
     }
 
-    public int[] arr9(int a, int delta, int first){
+    public int[] arr9(int a, int delta, int first) {
         arr = new int[a];
-        for(i=0; i<a; i++){
-            arr[i] = first+i*delta;
+        for (i = 0; i < a; i++) {
+            arr[i] = first + i * delta;
         }
         return arr;
     }
 
-    public double[] arr10(int a, int first, double q){
+    public double[] arr10(int a, int first, double q) {
         double[] arr = new double[a];
         arr[0] = first;
-        for(i=1; i<a; i++){
-            arr[i] = arr[i-1]/q;
+        for (i = 1; i < a; i++) {
+            arr[i] = arr[i - 1] / q;
         }
         return arr;
     }
 
-    public int[] arr11(int x){
+    public int[] arr11(int x) {
         int c = 0;
-        for(i=1;i<=x;i++){
-            if(x%i==0){
+        for (i = 1; i <= x; i++) {
+            if (x % i == 0) {
                 c++;
             }
         }
         arr = new int[c];
-        c=0;
-        for(i=1;i<=x;i++){
-            if(x%i==0){
+        c = 0;
+        for (i = 1; i <= x; i++) {
+            if (x % i == 0) {
                 arr[c] = i;
                 c++;
             }
@@ -121,67 +120,66 @@ public class Array {
         return arr;
     }
 
-    public Integer[] arr12(int x){
-        int c = 0;
-        ArrayList<Integer> arr= new ArrayList<>();
-        for(i=2;i<=x;i++){
-            c=0;
-            for(int j=1;j<=i;j++){
-                if(i%j==0){
+    public Integer[] arr12(int x) {
+        int c;
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (i = 2; i <= x; i++) {
+            c = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
                     c++;
                 }
             }
-            if(c==2){
+            if (c == 2) {
                 arr.add(i);
             }
         }
         return arr.toArray(new Integer[0]);
     }
 
-    public int[] arr13(int n){
-        arr = new int[2*n-1];
-        for (i=1;i<2*n;i++){
-            if(i>n){
-                arr[i-1] = 2*n-i;
-            }
-            else {
-                arr[i-1] = i;
+    public int[] arr13(int n) {
+        arr = new int[2 * n - 1];
+        for (i = 1; i < 2 * n; i++) {
+            if (i > n) {
+                arr[i - 1] = 2 * n - i;
+            } else {
+                arr[i - 1] = i;
             }
         }
         return arr;
     }
 
-    public void arr14(int[] a){
-        for(i=0;i<a.length;i++){
-            a[i]=-a[i];
+    public void arr14(int[] a) {
+        for (i = 0; i < a.length; i++) {
+            a[i] = -a[i];
         }
     }
 
-    public boolean arr15(int[] a, int k){
-        for(i=0; i<a.length; i++){
-            if(a[i]==k){
+    public boolean arr15(int[] a, int k) {
+        for (i = 0; i < a.length; i++) {
+            if (a[i] == k) {
                 return true;
             }
         }
         return false;
     }
 
-    public boolean arr16(Integer[] a){
-        for(i=0; i<a.length;i++){
-            if(a[i]==null){
+    public boolean arr16(Integer[] a) {
+        for (i = 0; i < a.length; i++) {
+            if (a[i] == null) {
                 return true;
             }
         }
         return false;
     }
 
-    public int arr17(int[] a){
-        int c=0;
-        for(i=0; i<a.length; i++){
-            if(a[i]==0){
-                c = c-1;
+    public int arr17(int[] a) {
+        int c = 0;
+        for (i = 0; i < a.length; i++) {
+            if (a[i] == 0) {
+                c = c - 1;
             }
-            if(a[i]%2==0){
+            if (a[i] % 2 == 0) {
                 c++;
             }
         }
--- src/test/java/ru/ssau/tk/way/practice2/arrays/ArrayTest.java
+++ src/test/java/ru/ssau/tk/way/practice2/arrays/ArrayTest.java
@@ -3,13 +3,12 @@ package ru.ssau.tk.way.practice2.arrays;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-
 import static org.testng.Assert.*;
 
 public class ArrayTest {
     Array arr = new Array();
     int[] a = new int[]{1, 2, -3};
+
     @Test
     public void testArr() {
 
@@ -18,40 +17,40 @@ public class ArrayTest {
 
     @Test
     public void testArr3() {
-        assertEquals(arr.arr3(3),new int[]{1, 3, 5});
+        assertEquals(arr.arr3(3), new int[]{1, 3, 5});
     }
 
     @Test
     public void testTestArr2() {
-        assertEquals(arr.arr2(3),new int[]{2, 1, 2});
+        assertEquals(arr.arr2(3), new int[]{2, 1, 2});
     }
 
     @Test
     public void testArr4() {
-        assertEquals(arr.arr4(3),new int[]{6, 4, 2});
+        assertEquals(arr.arr4(3), new int[]{6, 4, 2});
     }
 
     @Test
     public void testArr5() {
-        assertEquals(arr.arr5(5),new int[]{0, 1, 1, 2, 3});
+        assertEquals(arr.arr5(5), new int[]{0, 1, 1, 2, 3});
     }
 
     @Test
     public void testArr6() {
-        assertEquals(arr.arr6(5),new int[]{1, 4, 9, 16, 25});
+        assertEquals(arr.arr6(5), new int[]{1, 4, 9, 16, 25});
     }
 
     @Test
     public void testArr7() {
         Assert.assertEquals(arr.arr7(0, 1, 1), new double[]{-1}, 0.05);
-        Assert.assertEquals(arr.arr7(1, -7, 12),new double[]{4, 3},0.05);
+        Assert.assertEquals(arr.arr7(1, -7, 12), new double[]{4, 3}, 0.05);
         Assert.assertEquals(arr.arr7(1, -8, 16), new double[]{4}, 0.05);
         Assert.assertEquals(arr.arr7(1, 1, 17), new double[]{}, 0.05);
     }
 
     @Test
     public void testArr8() {
-        assertEquals(arr.arr8(7), new int[] {1,2,4,5,7,8,10});
+        assertEquals(arr.arr8(7), new int[]{1, 2, 4, 5, 7, 8, 10});
     }
 
     @Test
@@ -61,7 +60,7 @@ public class ArrayTest {
 
     @Test
     public void testArr10() {
-        assertEquals(arr.arr10(5, 10, 0.5),new double[]{10, 20, 40, 80, 160});
+        assertEquals(arr.arr10(5, 10, 0.5), new double[]{10, 20, 40, 80, 160});
     }
 
     @Test
@@ -71,7 +70,7 @@ public class ArrayTest {
 
     @Test
     public void testArr12() {
-        assertEquals(arr.arr12(5), new Integer[] {2, 3, 5});
+        assertEquals(arr.arr12(5), new Integer[]{2, 3, 5});
         assertEquals(arr.arr12(10), new Integer[]{2, 3, 5, 7});
     }
 
@@ -97,13 +96,13 @@ public class ArrayTest {
     @Test
     public void testArr16() {
         Integer[] a = new Integer[]{1, 3, null};
-        Integer[] b = new Integer[]{1,2,3};
+        Integer[] b = new Integer[]{1, 2, 3};
         assertTrue(arr.arr16(a));
         assertFalse(arr.arr16(b));
     }
 
     @Test
-    public void testArr17(){
+    public void testArr17() {
         int[] a = new int[]{2, 66, 3, 0};
         assertEquals(arr.arr17(a), 2);
     }
