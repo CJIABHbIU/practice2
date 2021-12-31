f2a7b5a1794e549fe4d62c0273ef1b6d6457050b
Kashapov <rusl.kashapov@gmail.com>
2021-12-31 14:26:55 +0400

    Exceptions 4.5 Test

---
+++ src/main/java/ru/ssau/tk/way/practice2/arrays/Matrix.java
@@ -0,0 +1,29 @@
+package ru.ssau.tk.way.practice2.arrays;
+
+public class Matrix {
+    private final double[][] matrix;
+    private final int n;
+    private final int m;
+
+    public Matrix(int n, int m) {
+        this.n = n;
+        this.m = m;
+        matrix = new double[n][m];
+    }
+
+    public int getM() {
+        return m;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public double getAt(int n, int m) {
+        return matrix[n][m];
+    }
+
+    public void setAt(int n, int m, double value) {
+        matrix[n][m] = value;
+    }
+}
